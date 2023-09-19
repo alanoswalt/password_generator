3def29172cb1d6b56ce785dacdf51ca56940f41f
Alan <alanoswalt01@gmail.com>
2023-09-18 19:40:04 -0600

    Add class and core functionality

--- password_gen.py
+++ password_gen.py
@@ -1,1 +1,31 @@
 #This file
+import string
+import random
+
+class generator:
+
+
+    def __init__(self, ammount):
+        self.ammount = ammount
+        self.x=""
+        self.y=0
+
+
+    def gen_pass(self):
+        
+        
+        while self.y < self.ammount:
+            self.x += random.choice(string.ascii_letters)
+            self.x +=str(random.randint(0,9))
+            self.y +=1
+        print(self.x)
+        
+
+
+
+
+new_generation = generator(15)
+
+new_generation.gen_pass()
+
+
